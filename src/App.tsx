e17913e5ec519c0eeef528811fb6b18d6d34aed6
姜小鱼 <1847535232@qq.com>
2024-01-30 23:04:26 +0800

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,37 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useEffect, useMemo, useState} from 'react';
+import NumberDisplay from "./components/NumberDisplay/"
+import styles from './styles.module.scss'
+import dayjs from "dayjs";
 
+const getRandomNumber = ( min: number, max: number ) => {
+    return Math.floor(Math.random() * ( max - min + 1 ) + min)
+}
+
+
+// 1, 做一个垂直的0-9 的div
+// 2， 滚动搭到7 时，div 就要垂直移动到 7 * height 个单位
 function App() {
+    const [time, seTime] = useState<number>(Date.now().valueOf())
+  const [numberString, setNumberString] = useState<string>('123');
+
+    useEffect(() => {
+        setInterval(() => {
+            seTime(Date.now().valueOf())
+        },1000)
+    },[])
+    const timestr = useMemo(function ()  {
+        return dayjs(time).format('HH:mm:ss')
+    },[time])
+
+    useEffect(() => {
+        setInterval(() => {
+            setNumberString(getRandomNumber(100,999).toString())
+        },1000)
+    }, [])
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={styles.app}>
+      <NumberDisplay numberString={numberString} />
     </div>
   );
 }
